0842d512c14940cbedadf8db5edac30c93cef714
priya226 <50729440+priya226@users.noreply.github.com>
2019-05-21 12:39:01 +0530

    Add files via upload

---
+++ roulettewheel.m
@@ -0,0 +1,17 @@
+%choosing one parent from Roulette Wheel Selection method
+f=[1,8,7,9];
+s=0;
+for i=1:length(f)
+    s=s+f(i)
+end
+f=(1/s).*f;
+y=rand();
+if y<(1/s)
+    solution=1
+elseif y>(1/s)&y<(7/s)
+    solution=3
+elseif y>(7/s)&y<(8/25)
+    solution=2
+else y>(8/s)&y<(9/s)
+    solution=4
+end
